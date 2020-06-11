92d17a6b2f52b40fef5e34311d8edd3ef7707b17
GeraDNG <geradng@gmail.com>
2020-06-11 03:38:21 -0500

    Tarea para familiarizarse con Storyboard y Swift

---
+++ T03_Storyboard_iOS/storyboard/storyboard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  storyboard
+//
+//  Created by itzel naranjo🧸 on 05/05/20.
+//  Copyright © 2020 example.example. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
